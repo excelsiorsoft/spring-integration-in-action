da6be9abf5cfa562f70358eb67de59d17d1338b5
sleyzerzon <senya22@yahoo.com>
2015-12-14 18:01:54 -0500

    introduced spring test

---
+++ siia-examples/hello-world/src/test/java/siia/helloworld/channel/HelloWorldTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package siia.helloworld.channel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Simeon
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"context.xml"})
+public class HelloWorldTest {
+
+	
+	@Autowired @Qualifier("names")private MessageChannel channel;
+	
+	@Test
+  public void run() {
+	//String cfg = "siia/helloworld/channel/context.xml";
+    //ApplicationContext context = new ClassPathXmlApplicationContext(cfg);
+   // MessageChannel channel = context.getBean("names", MessageChannel.class);
+    Message<String> message = MessageBuilder.withPayload("World").build();
+    channel.send(message);
+  }
+}
---
+++ siia-examples/hello-world/src/test/java/siia/helloworld/gateway/HelloWorldTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package siia.helloworld.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * @author Simeon
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"context.xml"})
+public class HelloWorldTest {
+
+	@Autowired
+	private HelloService helloService;
+	
+	@Test
+  public void run() {
+    System.out.println(helloService.sayHello("World"));
+  }
+
+}
